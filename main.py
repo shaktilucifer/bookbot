f10764c69977e26a955892f95768ad18d2331a1a
shaktilucifer <shakticse@gmail.com>
2025-08-03 00:18:59 -0400

    add sys args for file path

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from stats import get_words, get_char_map, printReport
+import sys
 # print(len(get_words('books/frankenstein.txt')))
+# 'books/frankenstein.txt'
 
-dict = get_char_map(get_words('books/frankenstein.txt'))
+if len(sys.argv) < 2:
+    print("Usage: python3 main.py <path_to_book>")
 
-printReport(len(get_words('books/frankenstein.txt')), dict, 'books/frankenstein.txt')
+bookFilePath=sys.argv[1];
+
+
+dict = get_char_map(get_words(bookFilePath))
+
+printReport(len(get_words(bookFilePath)), dict, bookFilePath)
+
